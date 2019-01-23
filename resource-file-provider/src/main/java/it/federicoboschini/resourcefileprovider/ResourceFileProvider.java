f88845c678ec0d1f0c706c473a1df0b26479da33
Federico Boschini <federico@nearit.com>
2019-01-23 01:15:20 +0100

    feat(library): handle more folders/extensions + refactoring

--- resource-file-provider/src/main/java/it/federicoboschini/resourcefileprovider/ResourceFileProvider.java
+++ resource-file-provider/src/main/java/it/federicoboschini/resourcefileprovider/ResourceFileProvider.java
@@ -7,10 +7,14 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ShareCompat;
 import android.support.v4.content.FileProvider;
+import android.util.Log;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.IllegalFormatException;
 
 import it.federicoboschini.androidresourcefileprovider.R;
 
@@ -18,19 +22,25 @@ import it.federicoboschini.androidresourcefileprovider.R;
  * @author Federico Boschini
  */
 public class ResourceFileProvider {
+
     private static final String TAG = "ResourceFileProvider";
+
+    private static final String ANDROID_RES_URI = "android.resource://%1$s/%2$s";
 
     public static final String FOLDER_RAW = "raw";
     public static final String FOLDER_DRAWABLE = "drawable";
     public static final String FOLDER_MIPMAP = "mipmap";
 
     public static final String TYPE_AUDIO = "audio/*";
+    public static final String TYPE_IMAGE = "image/*";
+    public static final String TYPE_VIDEO = "video/*";
+    public static final String TYPE_PDF = "application/pdf";
 
-    private Activity activity;
-    private String directory;
-    private String fileName;
-    private String fileExtension;
-    private String fileType;
+    private final Activity activity;
+    private final String directory;
+    private final String fileName;
+    private final String fileExtension;
+    private final String fileType;
 
     private ResourceFileProvider(@NonNull Activity activity, @NonNull String directory, @NonNull String fileName, @NonNull String fileExtension, @NonNull String fileType) {
         this.activity = activity;
@@ -40,51 +50,65 @@ public class ResourceFileProvider {
         this.fileType = fileType;
     }
 
-    public void shareFile() {
+    public void shareFile() throws FileNotFoundException {
         int resId = activity.getResources().getIdentifier(fileName, directory, activity.getPackageName());
-        InputStream inputStream;
-        switch (directory) {
-            case FOLDER_RAW:
-                inputStream = activity.getResources().openRawResource(resId);
-                break;
-            case FOLDER_DRAWABLE:
-                inputStream = activity.getResources().openRawResource(resId);
-                break;
-            case FOLDER_MIPMAP:
-                inputStream = activity.getResources().openRawResource(resId);
-                break;
-            default:
-                inputStream = activity.getResources().openRawResource(resId);
-                break;
-        }
-        try {
-            byte[] buff = new byte[1024];
-            int len;
-            FileOutputStream outputStream = activity.openFileOutput(fileName.concat(".").concat(fileExtension), Context.MODE_PRIVATE);
-            while ((len = inputStream.read(buff)) > 0) {
-                outputStream.write(buff, 0, len);
+        if (isValidResId(resId)) {
+            InputStream inputStream = null;
+            switch (directory) {
+                case FOLDER_RAW:
+                    inputStream = activity.getResources().openRawResource(resId);
+                    break;
+                case FOLDER_MIPMAP:
+                case FOLDER_DRAWABLE:
+                    Uri fileUri = null;
+                    try {
+                        fileUri = Uri.parse(String.format(ANDROID_RES_URI, activity.getPackageName(), resId));
+                    } catch (IllegalFormatException ignored) { }
+                    if (fileUri != null) {
+                        inputStream = activity.getContentResolver().openInputStream(fileUri);
+                    }
+                    break;
+                default:
+                    inputStream = activity.getResources().openRawResource(resId);
+                    break;
             }
-            inputStream.close();
-            outputStream.close();
-        } catch (Exception e) {
+            try {
+                byte[] buff = new byte[1024];
+                int len;
+                FileOutputStream outputStream = activity.openFileOutput(fileName.concat(".").concat(fileExtension), Context.MODE_PRIVATE);
+                if (inputStream != null) {
+                    while ((len = inputStream.read(buff)) > 0) {
+                        outputStream.write(buff, 0, len);
+                    }
+                    inputStream.close();
+                }
+                outputStream.close();
+            } catch (IOException e) {
+                Log.e(TAG, e.getLocalizedMessage());
+            }
 
-        }
-        
-        Uri uri = FileProvider.getUriForFile(activity, activity.getResources().getString(R.string.rfp_provider_authority), new File(activity.getFilesDir(), fileName.concat(".").concat(fileExtension)));
+            Uri uri = FileProvider.getUriForFile(activity, activity.getResources().getString(R.string.rfp_provider_authority), new File(activity.getFilesDir(), fileName.concat(".").concat(fileExtension)));
 
-        Intent intent = ShareCompat.IntentBuilder.from(activity).getIntent();
-        intent.setAction(Intent.ACTION_SEND);
-        intent.setType(fileType);
-        intent.putExtra(Intent.EXTRA_STREAM, uri);
-        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Intent intent = ShareCompat.IntentBuilder.from(activity).getIntent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.setType(fileType);
+            intent.putExtra(Intent.EXTRA_STREAM, uri);
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
-        if (intent.resolveActivity(activity.getPackageManager()) != null) {
-            activity.startActivity(intent);
+            if (intent.resolveActivity(activity.getPackageManager()) != null) {
+                activity.startActivity(intent);
+            }
+        } else {
+            throw new FileNotFoundException();
         }
     }
 
+    private boolean isValidResId(int resId) {
+        return resId != 0;
+    }
+
     public static class Builder {
-        private Activity activity;
+        private final Activity activity;
         private String directory;
         private String fileName;
         private String fileExtension;
